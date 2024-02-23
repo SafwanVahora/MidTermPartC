0877301be970ec2c2c312736e32a5d6d832133c4
Mohammed Safwan Vahora <vahora0@sheridancollege.ca>
2024-02-23 17:18:02 -0500

    Favorite subject Math

---
+++ src/SafwanDesign.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ * This class shows my favorite subject "MATHS"
+ * @author NAAZMIN
+ */
+public class SafwanDesign {
+    
+}
